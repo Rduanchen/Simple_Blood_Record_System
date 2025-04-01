4f2fff33943100b00bff0489acda096262a93561
Justin <chenyouduan@gmail.com>
2025-04-01 02:25:26 +0100

    fix import

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/js/vue.js', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/vue.js'));
+});
+
+app.get('/js/axios.js', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/axios.js'));
+});
+
+app.get('/js/flatpickr.css', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/flatpickr.css'));
+});
+
+app.get('/js/flatpickr.js', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/flatpickr.js'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
